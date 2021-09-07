16f61c1c2a86e08cccfef1fc5aad3393e62ae816
tcpgnl <i@tcpgnl.com>
2021-09-07 22:10:58 +0800

    [tcpgnl] chore: delete useless annotation

--- src/main/java/io/beiji/xdns/config/RedisConfig.java
+++ src/main/java/io/beiji/xdns/config/RedisConfig.java
@@ -1,11 +1,9 @@
 package io.beiji.xdns.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -13,7 +11,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
-@ConditionalOnClass(RedisOperations.class)
 public class RedisConfig {
 
     @Bean
@@ -22,7 +19,7 @@ public class RedisConfig {
             RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
 
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        var jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         template.setValueSerializer(jackson2JsonRedisSerializer);
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
         template.setKeySerializer(new StringRedisSerializer());
@@ -35,7 +32,7 @@ public class RedisConfig {
     @ConditionalOnMissingBean(StringRedisTemplate.class)
     public StringRedisTemplate stringRedisTemplate(
             RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
+        var template = new StringRedisTemplate();
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
