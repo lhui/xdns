f5b7d32e2f6c8f69225721e451bb4fde63a73cd1
tcpgnl <i@tcpgnl.com>
2021-09-16 23:45:29 +0800

    [tcpgnl] feat: change response for frontend

--- src/main/java/io/beiji/xdns/dns/DNSModel.java
+++ src/main/java/io/beiji/xdns/dns/DNSModel.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Data
 @Getter
 @Setter
-public class DNSModel {
+public class DNSModel implements Serializable {
     private String dns_name;
     private String dns_type;
     private String dns_content;
@@ -16,6 +18,29 @@ public class DNSModel {
         this.dns_name = dns_name;
         this.dns_type = dns_type;
         this.dns_content = dns_content;
+    }
 
+    public String getDns_name() {
+        return dns_name;
+    }
+
+    public void setDns_name(String dns_name) {
+        this.dns_name = dns_name;
+    }
+
+    public String getDns_type() {
+        return dns_type;
+    }
+
+    public void setDns_type(String dns_type) {
+        this.dns_type = dns_type;
+    }
+
+    public String getDns_content() {
+        return dns_content;
+    }
+
+    public void setDns_content(String dns_content) {
+        this.dns_content = dns_content;
     }
 }
--- src/main/java/io/beiji/xdns/dns/DNSResponse.java
+++ src/main/java/io/beiji/xdns/dns/DNSResponse.java
@@ -1,14 +1,27 @@
 package io.beiji.xdns.dns;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class DNSResponse {
+@Getter
+@Setter
+public class DNSResponse implements Serializable {
     private List<DNSModel> dnsModelList;
 
     public DNSResponse(List<DNSModel> dnsModelList) {
+        this.dnsModelList = dnsModelList;
+    }
+
+    public List<DNSModel> getDnsModelList() {
+        return dnsModelList;
+    }
+
+    public void setDnsModelList(List<DNSModel> dnsModelList) {
         this.dnsModelList = dnsModelList;
     }
 }
